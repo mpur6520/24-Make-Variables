8d042815b03453e48fdced9b739924172caef0db
Maru Puran <144689560+mpur6520@users.noreply.github.com>
2024-03-17 19:25:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Variable Assignment
+
+# Assign your name and your favourite food to 2 separate variables with suitable names.
+name="Maru"
+food1="spaghetti"
+food2="soup"
+
+############# SIMPLE ###################
+# Output the contents of the variables on 2 separate lines
+print(food1 + "\n" + food2)
+
+############# MEDIUM ####################
+# Output two sentences (not just the contents of the variables). The first with your name, the second with your favourite food.
+
+print("Hello, my name is " + name + ".")
+print("My favorite foods are " + food1 + " and " + food2 + ".")
+
+
+############# COMPLEX ###################
+# Output both pieces of information as part of the same sentence.
+# Make sure that you have spaces and punctuation in the correct places. 
+
+print("Hello! My name is " + name + " and my favorite foods are " + food1 + " and " + food2 + ".")
